068f0910d68a71ce98bf23fe040b4f9179b094c5
nyirenda95 <jonathannyirenda95@gmail.com>
2022-09-08 15:42:35 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main-prints a text
+ *
+ * Return always 0 (success)
+ */
+int main(void)
+{
+	printf("correct spellings,and the outcome is great,\n");
+	return(0);
+}
