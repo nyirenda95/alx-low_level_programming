0a5983bd70bf244bb7c27cb22096ebe9dd82b68d
nyirenda95 <jonathannyirenda95@gmail.com>
2022-09-08 16:16:59 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * main-prints a text
+ *
+ * Return:Always 1 (sucess)
+ */
+int main(void)
+{
+	write(2,"inever expect to see a perfect work from an imperfect man\".Alexander Hamilton 1905-05-18\n",20)
+		return(1);
+}
