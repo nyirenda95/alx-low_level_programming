de954a1801d047f8920b989757c73f6c79f45202
nyirenda95 <jonathannyirenda95@gmail.com>
2022-09-08 17:15:42 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main-print the string  in the put file
+ * main-print in c 
  *
  * Return: O (successful)
  */
 int main(void)
 {
-	putsi("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
