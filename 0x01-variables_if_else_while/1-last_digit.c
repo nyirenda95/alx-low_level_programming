0e4fe8702aa941ee465e6a2707a03c690db8881d
nyirenda95 <jonathannyirenda95@gmail.com>
2022-09-11 09:08:17 -0700

    Last digits

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,33 +1,37 @@
 #include <stdio.h>
+#include <time.h>
 
+/* more headers goes there*/
+/* betty style doc for function main goes there*/
 /**
- * main - Prints all possibe combinations of two two-digit numbers,
- * ranging from 0-99, separated by a comma followed by space
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
+ *
  */
 int main(void)
 {
-	int num1, num2;
+	int n;
 
-	for (num1 = 0; num1 <= 98; num2++)
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/*your code goes there*/
+	#include <stdio.h>
+	int lastDigit = n % 10;
+	char *str = "Last digit of";
+
+	if (lastDigit > 5)
 	{
-		for (num2 = num1 + 1; num2 <= 99; num2++)
-		{
-			putchar((num1 / 10) + '0');
-			putchar((num1 % 10) + '0');
-			putchar(' ');
-			putchar((num2 / 10) + '0');
-			putchar((num2 % 10) + '0');
-
-			if (num1 == 98 && num2 == 99)
-				continue;
-			
-			putchar(',');
-			putchar(' ');
-		}
+		printf("LAst digit of %d and is greater than 5\n", n, lastDigit);
 	}
-
-	putchar('\n');
+	else if (lastDigit == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+	}
+	else if (lastDigit < 6 && lastDigit != 0)
+	{
+		printf("%s %d is %d and is less than 6 and not 0\n", str, n, lastDigit);
+	}
 	return (0);
-}	
+}
+
