d6e628f1f3acb778b87efb232bd055b5bafcccb3
nyirenda95 <jonathannyirenda95@gmail.com>
2022-10-06 02:45:25 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,5 @@
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *allay_range(int min, int max);
+void *_relloc(void *ptr, unsigned int old_size, unsigned int new_size);
