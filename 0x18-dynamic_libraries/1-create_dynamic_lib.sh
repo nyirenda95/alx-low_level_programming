d9fa7c466adfcd07cec6c93ff5bdedb3495e365b
nyirenda95 <jonathannyirenda95@gmail.com>
2022-12-13 05:18:13 -0800

    1-create_dynamic_lib.sh

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-wget -O /tmp/win.so https://github.com/Zevrov/low_level_programming/raw/master/0x18-dynamic_libraries/win.so
-export LD_PRELOAD=/tmp/win.so
+gcc -Wall -Werror -Wextra -pedantic -fPIC *.c -shared -o liball.so
