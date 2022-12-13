3df5ad1ed2e428abb270cade163249ae9adc7068
nyirenda95 <jonathannyirenda95@gmail.com>
2022-12-13 03:25:09 -0800

    maths.c

--- 0x18-dynamic_libraries/maths.c
+++ 0x18-dynamic_libraries/maths.c
@@ -1,7 +1,10 @@
+#include <stdio.h>
+
 int add(int a, int b)
 {
 	return (a + b);
 }
+
 int sub(int a, int b)
 {
 	return (a - b);
@@ -16,7 +19,9 @@ int mul(int a, int b)
 {
 	return (a * b);
 }
+
 int div(int a, int b)
 {
 	return (a / b);
 }
+
