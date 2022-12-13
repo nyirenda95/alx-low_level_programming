b4da0caa008a7bb87f8e6c582805280e0358742c
nyirenda95 <jonathannyirenda95@gmail.com>
2022-12-12 23:21:42 -0800

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/Pericles001/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD =/tmp/nrandom.so
+export LD_PRELOAD=../libhack.so
